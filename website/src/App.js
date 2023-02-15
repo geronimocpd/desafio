6ea3bffbbc07dbb2a540a4b14af12945e562539d
Geronimo <geronimocpd@gmail.com>
2023-02-14 23:23:36 -0300

    criando estrutura do formulário para cadastro de nova entrega

--- website/src/App.js
+++ website/src/App.js
@@ -1,23 +1,76 @@
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
+const formReducer = (state, event) => {
+    if(event.reset) {
+        return {
+            count: 0,
+            name: '',
+            endereco: '',
+        }
+    }
+    return {
+        ...state,
+        [event.name]: event.value
+    }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [formData, setFormData] = useReducer(formReducer, {
+        count: 100,
+    });
+    const [submitting, setSubmitting] = useState(false);
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        setSubmitting(true);
+
+        setTimeout(() => {
+            setSubmitting(false);
+            setFormData({
+                reset: true
+            })
+        }, 3000);
+    }
+
+    const handleChange = event => {
+        const isCheckbox = event.target.type === 'checkbox';
+        setFormData({
+            name: event.target.name,
+            value: isCheckbox ? event.target.checked : event.target.value,
+        })
+    }
+    return(
+        <div className="wrapper">
+            {submitting &&
+                <div>
+                    You are submitting the following:
+                    <ul>
+                        {Object.entries(formData).map(([name, value]) => (
+                            <li key={name}><strong>{name}</strong>: {value.toString()}</li>
+                        ))}
+                    </ul>
+                </div>
+            }
+            <form onSubmit={handleSubmit}>
+                <fieldset disabled={submitting}>
+                    <label>
+                        <p>Nome</p>
+                        <input name="name" onChange={handleChange} value={formData.name || ''}/>
+                    </label>
+                    <label>
+                        <p>Peso</p>
+                        <input type="number" name="count" onChange={handleChange} step="1" value={formData.count || ''}/>
+                    </label>
+                    <label>
+                        <p>Endereço</p>
+                        <input  name="endereco" onChange={handleChange} value={formData.endereco || ''} />
+                    </label>
+                </fieldset>
+                <button type="submit" disabled={submitting}>Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default App;
